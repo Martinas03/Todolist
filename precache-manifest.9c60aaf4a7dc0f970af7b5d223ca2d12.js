72b3c17798b549bf07ce3065567805831d1e2dda
Martinas <martinas1990@mail.ru>
2023-08-21 16:25:28 +0300

    Updates

---
+++ precache-manifest.9c60aaf4a7dc0f970af7b5d223ca2d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fdd3e92d50d75d4cc78f770c3a2df1",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "e86548adb723a73d2b05",
+    "url": "/Todolist/static/css/main.84802909.chunk.css"
+  },
+  {
+    "revision": "82ce7370647c5a2dd822",
+    "url": "/Todolist/static/js/2.1f3e9603.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/Todolist/static/js/2.1f3e9603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86548adb723a73d2b05",
+    "url": "/Todolist/static/js/main.a965418a.chunk.js"
+  },
+  {
+    "revision": "23a8ead80f8150e06955",
+    "url": "/Todolist/static/js/runtime-main.ba498293.js"
+  }
+]);
